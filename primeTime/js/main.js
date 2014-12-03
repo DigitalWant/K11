aa1132dc126cdc61c1deef003b5439aa4b9deddb
Mike Dai <dai.mike@gmail.com>
2014-12-03 14:42:06 +0800

    weixinBrigde

--- primeTime/js/main.js
+++ primeTime/js/main.js
@@ -157,7 +157,66 @@ function showTips() {
 
     }
 }
+    var imgUrl = '../img/wechat.jpg';
+    var descContent = "盛时钟表";
+    var shareTitle = '人生起航，有我相伴';
+    //var// appid = 'wx4d1cefa551fd9f81';
 
+    function shareFriend() {
+        WeixinJSBridge.invoke('sendAppMessage', {
+            //"appid":// appid,
+            "img_url": imgUrl,
+            "img_width": "320",
+            "img_height": "480",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function (res) {
+            _report('send_msg', res.err_msg);
+        })
+    }
+    function shareTimeline() {
+        WeixinJSBridge.invoke('shareTimeline', {
+            //"appid":// appid,
+            "img_url": imgUrl,
+            "img_width": "320",
+            "img_height": "480",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function (res) {
+            _report('timeline', res.err_msg);
+        });
+    }
+    function shareWeibo() {
+        WeixinJSBridge.invoke('shareWeibo', {
+            "content": descContent,
+            "url": lineLink,
+        }, function (res) {
+            _report('weibo', res.err_msg);
+        });
+    }
+
+    // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+
+        // 发送给好友
+        WeixinJSBridge.on('menu:share:appmessage', function (argv) {
+            shareFriend();
+
+        });
+
+        // 分享到朋友圈
+        WeixinJSBridge.on('menu:share:timeline', function (argv) {
+            shareTimeline();
+
+        });
+
+        // 分享到微博
+        WeixinJSBridge.on('menu:share:weibo', function (argv) {
+            shareWeibo();
+        });
+    }, false);
 //onscreen test 
 /*
 $('.content').onScreen({
