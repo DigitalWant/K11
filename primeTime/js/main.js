8955761f7c066033f8335a471365415f6de52724
Mike Dai <dai.mike@gmail.com>
2014-12-04 16:23:16 +0800

    as

--- primeTime/js/main.js
+++ primeTime/js/main.js
@@ -227,6 +227,192 @@ function showTips() {
 
 
 
+  _audioNode    : $('.u-audio'),            // 声音模块
+  _audio      : null,                 // 声音对象
+  _audio_val    : true,                 // 声音是否开启控制
+
+/**
+ *  media资源管理
+ *  -->绑定声音控制事件
+ *  -->函数处理声音的开启和关闭
+ *  -->异步加载声音插件（延迟做）
+ *  -->声音初始化
+ *  -->视频初始化
+ *  -->声音和视频切换的控制
+ */
+  // 声音初始化
+  audio_init : function(){
+    // media资源的加载
+    var options_audio = {
+      loop: true,
+            preload: "auto",
+            src: car2._audioNode.attr('data-src')
+    }
+    
+        car2._audio = new Audio(); 
+
+        for(var key in options_audio){
+            if(options_audio.hasOwnProperty(key) && (key in car2._audio)){
+                car2._audio[key] = options_audio[key];
+            }
+        }
+        car2._audio.load();
+  },
+
+  // 声音事件绑定
+  audio_addEvent : function(){
+    if(car2._audioNode.length<=0) return;
+
+    // 声音按钮点击事件
+    var txt = car2._audioNode.find('.txt_audio'),
+      time_txt = null;
+    car2._audioNode.find('.btn_audio').on('click',car2.audio_contorl);
+
+    // 声音打开事件
+    $(car2._audio).on('play',function(){
+      car2._audio_val = false;
+
+      audio_txt(txt,true,time_txt);
+      $('.audio_open').removeClass('audio_close');
+      
+      // 开启音符冒泡
+      //$.fn.coffee.start();
+      //$('.coffee-steam-box').show(500);
+    })
+
+    // 声音关闭事件
+    $(car2._audio).on('pause',function(){
+      audio_txt(txt,false,time_txt)
+      $('.audio_open').addClass('audio_close');
+      
+      // 关闭音符冒泡
+      //$.fn.coffee.stop();
+      //$('.coffee-steam-box').hide(500);
+    })
+
+    function audio_txt(txt,val,time_txt){
+      if(val) txt.text('打开');
+      else txt.text('关闭');
+
+      if(time_txt) clearTimeout(time_txt);
+
+      txt.removeClass('z-move z-hide');
+      time_txt = setTimeout(function(){
+        txt.addClass('z-move').addClass('z-hide');
+      },1000)
+    }
+  },
+
+  // 声音控制函数
+  audio_contorl : function(){
+    if(!car2._audio_val){
+      car2.audio_stop();
+    }else{
+      car2.audio_play();
+    }
+  },  
+
+  // 声音播放
+  audio_play : function(){
+    car2._audio_val = false;
+    if(car2._audio) car2._audio.play();
+  },
+
+  // 声音停止
+  audio_stop  : function(){
+    car2._audio_val = true;
+    if(car2._audio) car2._audio.pause(); 
+  },
+
+  // 视频初始化
+  video_init : function(){
+    // 视频
+        $('.j-video').each(function(){
+          var option_video = {
+            controls: 'controls',
+            preload : 'none',
+            // poster : $(this).attr('data-poster'),
+            width : $(this).attr('data-width'),
+            height : $(this).attr('data-height'),
+            src : $(this).attr('data-src')
+          }
+
+          var video = $('<video class="f-hide"></video>')[0];
+
+          for(var key in option_video){
+                if(option_video.hasOwnProperty(key) && (key in video)){
+                    video[key] = option_video[key];
+                }
+                this.appendChild(video);
+            }
+
+            var img = $(video).prev();
+
+            $(video).on('play',function(){
+              img.hide();
+              $(video).removeClass('f-hide');
+            });
+
+            $(video).on('pause',function(){
+              img.show();
+              $(video).addClass('f-hide');
+            });
+        })
+
+        $('.j-video .img').on('click',function(){
+          var video = $(this).next()[0];
+
+          if(video.paused){
+            $(video).removeClass('f-hide');
+            video.play();
+            $(this).hide();
+          }
+        })
+  },
+
+  //处理声音和动画的切换
+  media_control : function(){
+    if(!car2._audio) return;
+    if($('video').length<=0) return;
+
+    $(car2._audio).on('play', function(){
+      $('video').each(function(){
+        if(!this.paused){
+          this.pause();
+        }
+      }); 
+    });
+
+    $('video').on('play', function(){
+      if(!car2._audio_val){
+        car2.audio_contorl();     
+      }
+    });
+  },
+
+  // media管理初始化
+  media_init : function(){
+    // 声音初始化
+    car2.audio_init();
+
+        // 视频初始化
+        car2.video_init();
+
+    // 绑定音乐加载事件
+    car2.audio_addEvent();
+
+    // 音频切换
+    car2.media_control();
+  };
+// 播放声音
+    if(!car2._audio) return;
+    car2._audioNode.removeClass('f-hide');
+    car2._audio.play();
+
+    // 声音启动
+    $(document).one("touchstart", function(){
+            car2._audio.play();
+        });
 
 /*
 
